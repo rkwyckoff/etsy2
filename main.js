20768f28da3b2035ccca1a95f4baa8fccb774e92
Kurt Wyckoff <kurtitservices@gmail.com>
2017-02-11 15:34:53 -0500

    Built JS script that puts etsy data onto the page via 2 function calls

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 
-  //function placeBox() {
-
+  function placeBox() {
 
     for (var i =0; i < 16; i++) {
-      var myHtml = boxTemplate(product);
-      $(".img-container").append(`<div class="imageBox"><img src ="${etsyData.results[i].Images[0].url_170x135}"></div>`);
+      var product = etsyData.results[i];
+      var etsyBoxes = boxTemplate(product);
+      // console.log(etsyBoxes);
+       $(".img-container").append(etsyBoxes)
+  }
 
-  function boxTemplate (product) {
-    return `
-    <div class="imageBox">
-    <img src="${product.Images[0].url_170x135}" />
-    <p>${product.title}</p>
-    <p>${product.Shop.shop_name}</p>
-    <p>${product.price}</p>
-  </div>
-  `;
-}
-}
-//}
-//}
-//placeBox(myHtml)
+  function boxTemplate(product) {
+
+      return `
+      <div class="imageBox">;
+      <img src="${product.Images[0].url_170x135}" />;
+      <p>${product.title}</p>;
+      <p>${product.Shop.shop_name}</p>;
+      <p>${product.price}</p>;
+      </div>`;
+   }
+  }
+   placeBox();
